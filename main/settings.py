9e8847931a00b674f4119c54b6bbbe7efd774a92
Rodrigo Neumann de Souza <rneumann@MacBook-Pro-de-Rodrigo.local>
2020-04-04 16:48:47 +0100

    added account app and linked index.html to base template, settings fixed

--- main/settings.py
+++ main/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
