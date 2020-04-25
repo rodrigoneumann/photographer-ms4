38cb8eaf993269f7e9d9b547fa813b49eb1e2499
Rodrigo Neumann de Souza <rneumann@MacBook-Pro-de-Rodrigo.local>
2020-04-25 10:57:33 +0100

    AWS configuration added to settings

--- main/settings.py
+++ main/settings.py
@@ -163,4 +163,14 @@ EMAIL_USE_TLS = True
 
 # AWS S3 Bucket Configuration
 
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = 'photographer-static'
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+AWS_S3_REGION_NAME = "eu-west-2"
+AWS_DEFAULT_ACL = None
+AWS_LOCATION = 'static'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
