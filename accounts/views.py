9608dc8ea4ba13fbd39311e904a6b582f8e1e6f3
Rodrigo Neumann de Souza <rneumann@MacBook-Pro-de-Rodrigo.local>
2020-06-05 14:49:40 +0100

    added field that inform when the subscription expires on profile page

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.utils import timezone
 from proServices.models import UserEditingPlans, VideoEditingPlans
 from .forms import RegisterForm, EditProfileForm
 
 
 def register(request):
-    # check if user is autthenticated
+    # check if user is authenticated
     if request.user.is_authenticated:
         return redirect(reverse("index"))
 
@@ -23,21 +24,46 @@ def register(request):
 
 @login_required
 def profile(request):
-    
-    #check if user is authentited  
+
+    # check if user is authenticated
     if request.user.is_authenticated is False:
         messages.error(
             request, "You are not authenticated, please login to view this page"
         )
         return redirect("login")
-    
+
     # check subscription
     is_subscribed = UserEditingPlans.objects.filter(user=request.user).first()
     if is_subscribed != None:
+
         subscription = is_subscribed.editing_plan
-        plan = VideoEditingPlans.objects.get(type=subscription)
+        plan = str(VideoEditingPlans.objects.get(type=subscription))
+        status = is_subscribed.is_active
+
+        if plan == "Weekly Plan":
+            date_added = is_subscribed.date_added
+            end = date_added + timezone.timedelta(days=7)
+            daysRemaining = (end - timezone.now()).days
+            if daysRemaining == 0:
+                subscription = None
+            else:
+                end = str(end.date())
+                status = "Your subscription is valid until : " + end
+
+        if plan == "Monthly Plan":
+            date_added = is_subscribed.date_added
+            end = date_added + timezone.timedelta(days=30)
+            daysRemaining = (end - timezone.now()).days
+            if daysRemaining == 0:
+                subscription = None
+            else:
+                end = str(end.date())
+                status = "Your subscription is valid until : " + end
+
         return render(
-            request, "profile.html", {"subscription": subscription, "plan": plan}
+            request,
+            "profile.html",
+            {"subscription": subscription, "plan": plan, "status": status},
         )
 
     return render(request, "profile.html")
@@ -45,7 +71,7 @@ def profile(request):
 
 @login_required
 def edit_profile(request):
-    
+
     if request.method == "POST":
         form = EditProfileForm(request.POST, instance=request.user)
 
